060b5f31cf48cd83238baa3f95e609cc185aa672
woo3145 <lcwoo3145@gmail.com>
2024-01-30 14:43:37 +0900

    ✨ Timer 페이지 모달 방식으로 변경

--- src/components/modals/Timer.tsx
+++ src/components/modals/Timer.tsx
@@ -1,50 +1,55 @@
-import { buttonVariants } from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 import { formatTime } from '@/lib/date-format';
-import { cn } from '@/lib/utils';
 import { PauseIcon } from '@radix-ui/react-icons';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 
 interface Props {
   initialTime: number;
+  isOpen: boolean;
+  onClose: () => void;
 }
 
-export const Timer = ({ initialTime }: Props) => {
+export const Timer = ({ initialTime, isOpen, onClose }: Props) => {
   const [curTime, setCurTime] = useState(initialTime);
 
   useEffect(() => {
+    if (!isOpen) return;
     const interval = setInterval(() => {
       setCurTime((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [isOpen]);
+
+  if (!isOpen) return null;
 
   return (
-    <>
+    <div className="absolute top-0 z-10 w-full md:border md:w-[416px] md:h-[736px] md:rounded-md bg-background border-t-0">
       <div className="rounded-t-md rounded-b-3xl pt-safe-offset-14 h-full pb-safe">
-        <main className="h-full overflow-y-scroll scrollbar-hide pb-10">
+        <main className="h-full overflow-y-scroll scrollbar-hide">
           <img
             src="./pots/pot_2.png"
             alt="main"
             className="h-1/2 mx-auto animate-soft-bounce"
           />
-          <p className="text-center">맛있게 익히는중..</p>
-          <div className="flex items-center justify-center pt-4 h-1/4">
+          <div className="flex flex-col items-center justify-center pt-4 h-1/4 gap-4">
+            <p className="text-center">맛있게 익히는중..</p>
             <p className="text-7xl text-primary font-semibold">
               {formatTime(curTime)}
             </p>
           </div>
           <div className="flex items-center justify-center py-10 h-1/4">
-            <Link
-              to="/"
-              className={cn(buttonVariants({ variant: 'ghost' }), 'p-2 h-auto')}
+            <Button
+              type="button"
+              onClick={onClose}
+              variant="ghost"
+              className={'p-2 h-auto'}
             >
               <PauseIcon className="w-8 h-8" />
-            </Link>
+            </Button>
           </div>
         </main>
       </div>
-    </>
+    </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,18 @@
 import { HomeHeader } from '@/components/headers/HomeHeader';
-import { buttonVariants } from '@/components/ui/button';
+import { Timer } from '@/components/modals/Timer';
+import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { useCurrentMemberQuery } from '@/queries/memberQueries';
 import { EggInventorySection } from '@/sections/EggInventorySection';
 import { PlayIcon } from '@radix-ui/react-icons';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
 
 export const Home = () => {
   const { data, isPending } = useCurrentMemberQuery();
-  // const [studyTime, setStudyTime] = useState(0);
+  const [isTimerModalOpen, setIsTimerModalOpen] = useState(false);
 
-  // Timer 페이지 모달로 바꾸기
+  const openTimerModal = () => setIsTimerModalOpen(true);
+  const closeTimerModal = () => setIsTimerModalOpen(false);
 
   if (isPending || !data) {
     console.log('Pending');
@@ -25,15 +26,23 @@ export const Home = () => {
         <main className="h-full overflow-y-scroll scrollbar-hide pb-10">
           <img src="./pots/pot_2.png" alt="main" className="h-1/2 mx-auto" />
           <div className="flex items-center justify-center pt-4">
-            <p className="text-6xl text-primary font-semibold">1:30:00</p>
+            <p className="text-6xl text-primary font-semibold">1:00:00</p>
           </div>
           <div className="flex items-center justify-center py-10">
-            <Link
-              to="/study"
-              className={cn(buttonVariants({ variant: 'ghost' }), 'p-2 h-auto')}
+            <Button
+              onClick={openTimerModal}
+              variant={'ghost'}
+              className={cn('p-2 h-auto')}
             >
               <PlayIcon className="w-8 h-8" />
-            </Link>
+            </Button>
+            {isTimerModalOpen && (
+              <Timer
+                initialTime={3600}
+                isOpen={isTimerModalOpen}
+                onClose={closeTimerModal}
+              />
+            )}
           </div>
 
           <EggInventorySection memberId={data.member_id} />
