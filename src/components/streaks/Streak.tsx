0d5dfe245c6b5667c843add7801f4dbb35fc48f3
woo3145 <lcwoo3145@gmail.com>
2024-02-09 01:09:12 +0900

    ✨ 스트릭 UI/기능 작업(색상 추가, studyData 병합, 월 표시)

--- src/components/streaks/Streak.tsx
+++ src/components/streaks/Streak.tsx
@@ -1,4 +1,8 @@
-import { useMemo } from 'react';
+import { formatDateStr } from '@/lib/date-format';
+import { HeatMapData } from '@/lib/study-records';
+import { cn } from '@/lib/utils';
+import { IStudyStreak } from '@/models/streak.model';
+import { useEffect, useMemo } from 'react';
 
 const generateYearDates = () => {
   const dates = [];
@@ -11,17 +15,20 @@ const generateYearDates = () => {
   return dates;
 };
 
-const groupByWeeks = (dates: Date[]) => {
+const groupByWeeks = (dates: Date[], heatMapData: HeatMapData[]) => {
   const weeks: {
-    dates: Date[];
+    dates: { date: string; value?: number }[];
     month: number;
     isFirstWeek: boolean;
   }[] = [];
 
-  let week: Date[] = [];
+  let week: { date: string; value?: number }[] = [];
   let lastMonth: number | null = null;
+
   dates.forEach((date, idx) => {
     const month = date.getMonth();
+    const dateStr = formatDateStr(date);
+    const heatMapNode = heatMapData.find((data) => data.date === dateStr);
 
     // 주 시작이면 week객체 푸시 후 초기화
     if (date.getDay() === 0 && 0 < week.length) {
@@ -34,7 +41,7 @@ const groupByWeeks = (dates: Date[]) => {
 
       lastMonth = month;
     }
-    week.push(date);
+    week.push({ date: dateStr, value: heatMapNode ? heatMapNode.count : 0 });
     if (idx === dates.length - 1) {
       weeks.push({
         dates: week,
@@ -47,9 +54,37 @@ const groupByWeeks = (dates: Date[]) => {
   return weeks.reverse(); // 최신 데이터가 위로가도록 뒤집기
 };
 
-export const Streak = () => {
-  const dates = useMemo(() => generateYearDates(), []); // 12달치 Date객체 생성 ->
-  const weeks = useMemo(() => groupByWeeks(dates), [dates]); // 7개씩 묶기
+interface Props {
+  heatMapData: HeatMapData[];
+  streakInfo: IStudyStreak | null;
+}
+
+export const Streak = ({ heatMapData, streakInfo }: Props) => {
+  const dates = useMemo(() => generateYearDates(), []); // 12달치 Date객체 생성
+  const weeks = useMemo(
+    () => groupByWeeks(dates, heatMapData),
+    [dates, heatMapData]
+  ); // 7개씩 묶기 & heatMapData 병합
+
+  useEffect(() => {
+    if (!streakInfo?.palette) return;
+    document.documentElement.style.setProperty(
+      '--color-scale-1',
+      streakInfo.palette.light_color
+    );
+    document.documentElement.style.setProperty(
+      '--color-scale-2',
+      streakInfo.palette.normal_color
+    );
+    document.documentElement.style.setProperty(
+      '--color-scale-3',
+      streakInfo.palette.dark_color
+    );
+    document.documentElement.style.setProperty(
+      '--color-scale-4',
+      streakInfo.palette.darker_color
+    );
+  }, [streakInfo]);
 
   return (
     <div className="streak-container relative flex flex-col gap-2">
@@ -66,10 +101,17 @@ export const Streak = () => {
                 .map((_n, idx) => <div key={`null-${idx}`}></div>)
             : null}
           {week.dates.map((date) => {
+            const hours = date.value ? date.value / 3600 : 0;
             return (
               <div
-                key={date.toISOString()}
-                className="day w-full aspect-square bg-accent rounded-sm"
+                key={date.date}
+                className={cn(
+                  'day w-full aspect-square bg-accent rounded-sm',
+                  date.value && hours < 2 && 'color-scale-1',
+                  2 < hours && hours < 4 && 'color-scale-2',
+                  4 < hours && hours < 6 && 'color-scale-3',
+                  6 < hours && 'color-scale-4'
+                )}
               />
             );
           })}
--- src/sections/myPage/StreakSection.tsx
+++ src/sections/myPage/StreakSection.tsx
@@ -1,4 +1,3 @@
-import { StudyStreak } from '@/components/records/StudyStreak';
 import { Streak } from '@/components/streaks/Streak';
 import { HeatMapData } from '@/lib/study-records';
 import { IStudyStreak } from '@/models/streak.model';
@@ -13,9 +12,8 @@ export const StreakSection = ({ heatMapData, streakInfo }: Props) => {
     <section className="px-4">
       <p className="text-center text-sm font-bold pb-4">스트릭</p>
       <div className="flex items-center justify-center">
-        {/* <StudyStreak heatMapData={heatMapData} streakInfo={streakInfo} /> */}
         <div className="w-2/3">
-          <Streak />
+          <Streak heatMapData={heatMapData} streakInfo={streakInfo} />
         </div>
       </div>
     </section>
