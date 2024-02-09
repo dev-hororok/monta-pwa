ebf71e90bced5a72057a9435333b6461d18ed797
woo3145 <lcwoo3145@gmail.com>
2024-02-09 21:32:53 +0900

    ♻️ 캘린더 기능 리펙토링

---
+++ src/hooks/useCalendar.tsx
@@ -0,0 +1,94 @@
+import { useState, useMemo, useCallback } from 'react';
+import {
+  useStatisticHeatMapQuery,
+  useMonthlyStatisticQuery,
+} from '@/apis/queries/memberQueries';
+import { formatDateStr } from '@/lib/date-format';
+import { OnArgs, TileArgs } from 'react-calendar';
+
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+export const useCalendar = (memberId: string) => {
+  const [value, setValue] = useState<Value>(new Date());
+  const [year, setYear] = useState(new Date().getFullYear());
+  const [month, setMonth] = useState(new Date().getMonth());
+
+  const startDate = useMemo(() => new Date(year, month - 1, 23), [year, month]);
+  const endDate = useMemo(() => new Date(year, month + 1, 7), [year, month]);
+
+  const selectedDateStr = useMemo(() => {
+    if (value instanceof Date) {
+      return formatDateStr(value);
+    }
+    return formatDateStr(new Date());
+  }, [value]);
+
+  // react-calendar date 클릭 시 호출
+  const onChange = useCallback((newValue: Value) => {
+    setValue(newValue);
+  }, []);
+
+  // react-calendar 달/연도 변경 시 호출
+  const onActiveStartDateChange = useCallback(
+    ({ action, activeStartDate }: OnArgs) => {
+      if (
+        action === 'prev' ||
+        action === 'next' ||
+        action === 'prev2' ||
+        action === 'next2'
+      ) {
+        if (activeStartDate) {
+          setMonth(activeStartDate.getMonth());
+          setYear(activeStartDate.getFullYear());
+        }
+      }
+    },
+    []
+  );
+
+  const { data: heatMapData } = useStatisticHeatMapQuery(
+    memberId,
+    formatDateStr(startDate),
+    formatDateStr(endDate)
+  );
+  const { data: monthlyStatistic } = useMonthlyStatisticQuery(
+    memberId,
+    year,
+    month + 1
+  );
+
+  // heatMap 해시맵 (조회용)
+  const studiedDays = useMemo(() => {
+    const map = new Map<string, number>();
+    heatMapData?.forEach((data) => {
+      map.set(data.date, data.totalSeconds);
+    });
+    return map;
+  }, [heatMapData]);
+
+  // 타일의 date값이 heatMap에 존재할 시 스티커 붙여줌
+  const tileContent = useCallback(
+    ({ date, view }: TileArgs) => {
+      if (view === 'month') {
+        const dateStr = formatDateStr(date);
+        if (studiedDays.has(dateStr)) {
+          return <img src="/octopus.png" className="w-8 h-8 mx-auto" />;
+        } else {
+          return <div className="w-8 h-8" />;
+        }
+      }
+      return null;
+    },
+    [studiedDays]
+  );
+
+  return {
+    value,
+    onChange,
+    onActiveStartDateChange,
+    tileContent,
+    monthlyStatistic,
+    selectedDateStr,
+  };
+};
--- src/sections/myPage/CalendarSection.tsx
+++ src/sections/myPage/CalendarSection.tsx
@@ -1,57 +1,23 @@
 import Calendar from 'react-calendar';
-import { useMemo, useState } from 'react';
-import { formatDateStr, formatTime } from '@/lib/date-format';
+import { formatTime } from '@/lib/date-format';
+import { DailyStatisticSection } from './DailyStatisticSection';
+import { useCalendar } from '@/hooks/useCalendar';
 
 import '@/styles/calendar.css';
-import { DailyStatisticSection } from './DailyStatisticSection';
-import {
-  useMonthlyStatisticQuery,
-  useStatisticHeatMapQuery,
-} from '@/apis/queries/memberQueries';
 
 interface Props {
   memberId: string;
 }
 
-type ValuePiece = Date | null;
-
-type Value = ValuePiece | [ValuePiece, ValuePiece];
-
 export const CalendarSection = ({ memberId }: Props) => {
-  const [value, onChange] = useState<Value>(new Date());
-  const [year, setYear] = useState(new Date().getFullYear());
-  const [month, setMonth] = useState(new Date().getMonth());
-  const startDate = useMemo(() => new Date(year, month - 1, 23), [year, month]);
-  const endDate = useMemo(() => new Date(year, month + 1, 7), [year, month]);
-
-  const selectedDateStr = useMemo(() => {
-    if (value instanceof Date) {
-      return formatDateStr(value);
-    }
-    return formatDateStr(new Date());
-  }, [value]);
-
-  const { data: heatMapData } = useStatisticHeatMapQuery(
-    memberId,
-    formatDateStr(startDate),
-    formatDateStr(endDate)
-  );
-
-  const { data: monthlyStatistic } = useMonthlyStatisticQuery(
-    memberId,
-    year,
-    month + 1
-  );
-
-  const studiedDays = useMemo(() => {
-    const map = new Map<string, number>();
-    if (heatMapData) {
-      heatMapData.forEach((data) => {
-        map.set(data.date, data.totalSeconds);
-      });
-    }
-    return map;
-  }, [heatMapData]);
+  const {
+    value,
+    onChange,
+    selectedDateStr,
+    onActiveStartDateChange,
+    tileContent,
+    monthlyStatistic,
+  } = useCalendar(memberId);
 
   return (
     <section className="px-4">
@@ -60,29 +26,10 @@ export const CalendarSection = ({ memberId }: Props) => {
           value={value}
           onChange={onChange}
           view="month"
-          onActiveStartDateChange={({ action, activeStartDate }) => {
-            if (
-              action === 'prev' ||
-              action === 'next' ||
-              action === 'prev2' ||
-              action === 'next2'
-            ) {
-              if (activeStartDate) {
-                setMonth(activeStartDate.getMonth());
-                setYear(activeStartDate.getFullYear());
-              }
-            }
-          }}
+          onActiveStartDateChange={onActiveStartDateChange}
           formatDay={(_locale, date) => date.getDate().toString()}
           maxDate={new Date()} // 오늘까지만 조회가능
-          tileContent={({ date, view }) => {
-            const dateStr = formatDateStr(date);
-            return view === 'month' && studiedDays.has(dateStr) ? (
-              <img src="/octopus.png" className="w-8 h-8 mx-auto" />
-            ) : (
-              <div className="w-8 h-8" />
-            );
-          }}
+          tileContent={tileContent}
         />
         <p className="text-right text-sm text-foreground/70">
           <span>{formatTime(monthlyStatistic?.totalSeconds || 0)}</span> /
