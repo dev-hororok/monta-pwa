13a53df0e1eee82789703ada5ac106ee2ef69ed3
woo3145 <lcwoo3145@gmail.com>
2024-02-10 23:17:03 +0900

    ✨ 팔레트 뽑기 결과에 따라 폭죽 효과(react-canvas-confetti)

--- src/components/modals/paletteAcquisition/index.tsx
+++ src/components/modals/paletteAcquisition/index.tsx
@@ -8,6 +8,7 @@ import {
 } from '@/components/ui/alert-dialog';
 import { useModalStore } from '@/stores/useModalStore';
 import { Badge } from '@/components/ui/badge';
+import Fireworks from 'react-canvas-confetti/dist/presets/fireworks';
 
 export const PaletteAcquisitionDialog = () => {
   const { isOpen, data: palette } = useModalStore(
@@ -24,44 +25,57 @@ export const PaletteAcquisitionDialog = () => {
   }
 
   return (
-    <AlertDialog open={isOpen}>
-      <AlertDialogContent
-        className={cn(
-          `w-full md:max-w-[416px] max-h-[400px] flex flex-col items-center py-safe-offset-14 overflow-y-scroll scrollbar-hide`
-        )}
-      >
-        <AlertDialogTitle className="text-2xl">팔레트 변경</AlertDialogTitle>
+    <>
+      <AlertDialog open={isOpen}>
+        <AlertDialogContent
+          className={cn(
+            `w-full md:max-w-[416px] max-h-[400px] flex flex-col items-center py-safe-offset-14 overflow-y-scroll scrollbar-hide`
+          )}
+        >
+          {palette.grade === 'Epic' ? (
+            <Fireworks autorun={{ speed: 1, duration: 300 }} />
+          ) : null}
+          {palette.grade === 'Legendary' ? (
+            <Fireworks autorun={{ speed: 4, duration: 2000 }} />
+          ) : null}
+          <AlertDialogTitle className="text-2xl">팔레트 변경</AlertDialogTitle>
 
-        <div className="flex items-center gap-4">
-          <div
-            style={{ backgroundColor: palette.light_color }}
-            className="w-8 h-8"
-          />
-          <div
-            style={{ backgroundColor: palette.normal_color }}
-            className="w-8 h-8"
-          />
-          <div
-            style={{ backgroundColor: palette.dark_color }}
-            className="w-8 h-8"
-          />
-          <div
-            style={{ backgroundColor: palette.darker_color }}
-            className="w-8 h-8"
-          />
-        </div>
-        <div className="flex justify-center">
-          <Badge>{palette.grade} 등급</Badge>
-        </div>
-        <p className="text-center font-semibold text-lg py-4">{palette.name}</p>
-        <div className="w-full flex flex-col justify-end h-full gap-2">
-          <AlertDialogFooter className="w-full">
-            <AlertDialogAction onClick={handleOnClick} className="h-12 w-full">
-              확인
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </div>
-      </AlertDialogContent>
-    </AlertDialog>
+          <div className="flex items-center gap-4">
+            <div
+              style={{ backgroundColor: palette.light_color }}
+              className="w-8 h-8"
+            />
+            <div
+              style={{ backgroundColor: palette.normal_color }}
+              className="w-8 h-8"
+            />
+            <div
+              style={{ backgroundColor: palette.dark_color }}
+              className="w-8 h-8"
+            />
+            <div
+              style={{ backgroundColor: palette.darker_color }}
+              className="w-8 h-8"
+            />
+          </div>
+          <div className="flex justify-center">
+            <Badge>{palette.grade} 등급</Badge>
+          </div>
+          <p className="text-center font-semibold text-lg py-4">
+            {palette.name}
+          </p>
+          <div className="w-full flex flex-col justify-end h-full gap-2">
+            <AlertDialogFooter className="w-full">
+              <AlertDialogAction
+                onClick={handleOnClick}
+                className="h-12 w-full"
+              >
+                확인
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </div>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
