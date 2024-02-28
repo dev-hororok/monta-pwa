557cfc02966547d8a52b676f141c34191b087fb4
woo3145 <lcwoo3145@gmail.com>
2024-02-28 22:25:26 +0900

    🐛 구글 간편로그인 에러처리

--- src/components/forms/oauth-list.tsx
+++ src/components/forms/oauth-list.tsx
@@ -6,26 +6,32 @@ import { useAuthStore } from '@/stores/auth-store';
 import { toast } from 'sonner';
 import { useNavigate } from 'react-router-dom';
 import { API_URL_NEST } from '@/constants/constants';
+import { handleApiError } from '@/apis/common/api-error-handler';
 
 export const OAuthList = () => {
   const navigate = useNavigate();
   const googleSocialLogin = useGoogleLogin({
     flow: 'auth-code',
     onSuccess: async (data) => {
-      const response = await axios.post(
-        `${API_URL_NEST}/timer-api/auth/google/login`,
-        { code: data.code }
-      );
-      useAuthStore.getState().authenticate(
-        {
-          accessToken: response.data.data.access_token,
-          refreshToken: response.data.data.refresh_token,
-        },
-        response.data.data.expires_in
-      );
+      try {
+        const response = await axios.post(
+          `${API_URL_NEST}/timer-api/auth/google/login`,
+          { code: data.code }
+        );
+        useAuthStore.getState().authenticate(
+          {
+            accessToken: response.data.data.access_token,
+            refreshToken: response.data.data.refresh_token,
+          },
+          response.data.data.expires_in
+        );
 
-      toast.success('로그인에 성공하였습니다.');
-      navigate('/');
+        toast.success('로그인에 성공하였습니다.');
+        navigate('/');
+      } catch (e) {
+        const result = handleApiError(e);
+        toast.error(result.error);
+      }
     },
     onError: () => {
       toast.success('로그인에 실패하였습니다.');
