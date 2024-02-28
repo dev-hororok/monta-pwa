d940b30321d48add1cdbcad78fd3795fba742d30
woo3145 <lcwoo3145@gmail.com>
2024-02-29 03:40:38 +0900

    ✨ 카카오 로그인 콜백 구현

--- src/pages/auth/login/callback/kakao/index.tsx
+++ src/pages/auth/login/callback/kakao/index.tsx
@@ -1,3 +1,39 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
+
+import { handleApiError } from '@/apis/common/api-error-handler';
+import { API_URL_NEST } from '@/constants/constants';
+import { useAuthStore } from '@/stores/auth-store';
+
 export const KakaoLoginCallback = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get('code');
+    const fetch = async () => {
+      try {
+        const response = await axios.post(
+          `${API_URL_NEST}/timer-api/auth/kakao/login`,
+          { code }
+        );
+        useAuthStore.getState().authenticate(
+          {
+            accessToken: response.data.data.access_token,
+            refreshToken: response.data.data.refresh_token,
+          },
+          response.data.data.expires_in
+        );
+
+        toast.success('로그인에 성공하였습니다.');
+        navigate('/');
+      } catch (e) {
+        const result = handleApiError(e);
+        toast.error(result.error);
+        navigate('/auth/login');
+      }
+    };
+    fetch();
+  }, []);
   return null;
 };
