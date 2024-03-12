4c5deaf2a27e51b71b8169ae4ce2f9fbdf046a90
woo3145 <lcwoo3145@gmail.com>
2024-03-13 02:01:12 +0900

    ✨ 인벤토리 emptyMessage 추가

--- src/pages/inventory/_components/item-inventory-section/index.tsx
+++ src/pages/inventory/_components/item-inventory-section/index.tsx
@@ -13,18 +13,6 @@ export const ItemInventorySection = ({ member }: ItemInventorySectionProps) => {
     isError,
   } = useConsumableInventoryQuery(member.member_id);
 
-  if (isPending) {
-    return (
-      <section>
-        <p className="font-semibold pb-4">사용 아이템</p>
-        <div className="grid grid-cols-3 gap-2">
-          <ConsumableItemInventoryCard.Skeleton />
-          <ConsumableItemInventoryCard.Skeleton />
-          <ConsumableItemInventoryCard.Skeleton />
-        </div>
-      </section>
-    );
-  }
   if (isError) {
     return <div className="text-center text-red-500">오류가 발생했습니다.</div>;
   }
@@ -32,16 +20,53 @@ export const ItemInventorySection = ({ member }: ItemInventorySectionProps) => {
   return (
     <section>
       <p className="font-semibold pb-4">사용 아이템</p>
-      <div className="grid grid-cols-3 gap-2">
-        {items.map((item, idx) => {
-          return (
-            <ConsumableItemInventoryCard
-              key={idx}
-              consumableItemInventory={item}
-            />
-          );
-        })}
-      </div>
+      {isPending ? (
+        <div className="grid grid-cols-3 gap-2">
+          <ConsumableItemInventoryCard.Skeleton />
+          <ConsumableItemInventoryCard.Skeleton />
+          <ConsumableItemInventoryCard.Skeleton />
+        </div>
+      ) : (
+        <>
+          {items.length === 0 ? <EmptyItemMessage /> : null}
+          <div className="grid grid-cols-3 gap-2">
+            {items.map((item, idx) => {
+              return (
+                <ConsumableItemInventoryCard
+                  key={idx}
+                  consumableItemInventory={item}
+                />
+              );
+            })}
+          </div>
+        </>
+      )}
     </section>
+  );
+};
+
+const EmptyItemMessage = () => {
+  return (
+    <div className="flex-center flex-col py-4 gap-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-mood-sad size-8"
+        width="44"
+        height="44"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="#2c3e50"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+        <path d="M9 10l.01 0" />
+        <path d="M15 10l.01 0" />
+        <path d="M9.5 15.25a3.5 3.5 0 0 1 5 0" />
+      </svg>
+      <p className="text-sm">아이템이 없닭!</p>
+    </div>
   );
 };
