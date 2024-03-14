5fa23a30794672a3b00c031bed0ce4152956ee2f
woo3145 <lcwoo3145@gmail.com>
2024-03-14 16:53:07 +0900

    💄 로그인 페이지 이미지 변경

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -7,13 +7,13 @@ const LoginGatewayPage = () => {
   return (
     <div className="w-full h-full px-4 bg-background md:rounded-md pb-safe-offset-14 pt-safe-offset-14 overflow-hidden">
       <img
-        src="/chicken_2.png"
+        src="/chicken.png"
         alt="monta-main"
         className="h-1/2 p-10 aspect-square mx-auto"
       />
       <div className="h-1/2 flex flex-col items-center justify-between">
         <p className="text-4xl font-bold antialiased text-foreground tracking-wider">
-          뽀모도로닭
+          뽀모닭
         </p>
         <Link
           replace
