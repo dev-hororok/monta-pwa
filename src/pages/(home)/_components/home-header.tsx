6b9e20b4fdc8306600eea1391ab72f11b404a3d3
woo3145 <lcwoo3145@gmail.com>
2024-03-20 03:58:03 +0900

    Add conversion login link in home-header

--- src/pages/(home)/_components/home-header.tsx
+++ src/pages/(home)/_components/home-header.tsx
@@ -2,20 +2,24 @@ import { useCurrentMemberQuery } from '@/services/queries/member-queries';
 import { Button } from '../../../components/ui/button';
 import { Link } from 'react-router-dom';
 import { headerIcons } from '@/components/icons';
+import { cn } from '@/lib/utils';
 
 export const HomeHeader = () => {
   const { data } = useCurrentMemberQuery();
   return (
     <div className="flex justify-between items-end fixed top-0 left-0 right-0 md:absolute h-14 px-2 mt-safe md:rounded-t-md">
-      <Link to="/shop" replace>
+      <Link to={data ? '/shop' : '/auth/login'} replace>
         <Button
           type="button"
           variant={'ghost'}
-          className="flex items-center text-lg px-2"
+          className={cn(
+            'flex items-center px-2',
+            data ? 'text-lg' : 'text-sm text-blue-600 font-normal'
+          )}
           aria-label="shop"
         >
           <img src={headerIcons.feather} alt="point icon" className="size-6" />
-          {data?.point}
+          {data ? data.point : '로그인'}
         </Button>
       </Link>
       <Link to="/more" replace>
