8f056871ce9b22aac9c7ce9ba6f9461a9b3948ab
woo3145 <lcwoo3145@gmail.com>
2024-03-20 21:23:11 +0900

    Add easter-egg for admins to enter admin page

--- src/pages/more/_components/app-version-view.tsx
+++ src/pages/more/_components/app-version-view.tsx
@@ -1,9 +1,42 @@
+import * as React from 'react';
+
 import { morePageIcons } from '@/components/icons';
 import { APP_INFO } from '@/constants/app-info';
+import { useNavigate } from 'react-router-dom';
 
 export const AppVersionView = () => {
+  const [clickCount, setClickCount] = React.useState(0);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    let timerId: NodeJS.Timeout;
+
+    const resetCount = () => {
+      setClickCount(0);
+    };
+
+    if (clickCount > 0) {
+      timerId = setTimeout(resetCount, 5000);
+    }
+
+    return () => clearTimeout(timerId);
+  }, [clickCount]);
+
+  const handleInteraction = () => {
+    setClickCount((prevCount) => {
+      const newCount = prevCount + 1;
+      if (newCount === 10) {
+        navigate('/admin');
+      }
+      return newCount;
+    });
+  };
+
   return (
-    <div className="flex items-center justify-between w-full py-4 px-6 text-sm">
+    <div
+      onClick={handleInteraction}
+      className="flex items-center justify-between w-full py-4 px-6 text-sm"
+    >
       <div className="flex items-center gap-2">
         <img
           src={morePageIcons.info}
