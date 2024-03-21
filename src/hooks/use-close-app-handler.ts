cb1430a1799bb3dde360f422a32def394f9de7bd
woo3145 <lcwoo3145@gmail.com>
2024-03-21 17:55:58 +0900

    Simplify use-close-app-handler.ts & Add comments

--- src/hooks/use-close-app-handler.ts
+++ src/hooks/use-close-app-handler.ts
@@ -2,17 +2,19 @@ import { useEffect, useRef } from 'react';
 import { toast } from 'sonner';
 
 /** 뒤로가기를 {delay}초안에 연속하여 누르면 앱을 종료하는 훅
- *  - 뒤로가기 후 상호작용이 없다면 delay초가 지나서 누르더라도 앱이 종료됨
- *  - (히스토리를 조작한 후 사용자 상호작용이 없을 때 pop동작 시 해당 도메인 전체가 pop됨)
+ *  - 뒤로가기 후 상호작용이 없다면 delay초가 지나서 뒤로가기를 누르더라도 앱이 종료됨
+ *  - (프로그래밍 형식으로 조작된 히스토리는 사용자 상호작용이 없으면 pop동작 시 무시됨)
+ *
+ *  - 종료가 될 페이지에서 항상 이전 히스토리가 1개가 되도록 신경써야함
  */
 export const useCloseAppHandler = (delay = 1500) => {
-  const backButtonPressed = useRef(false);
+  const isBackButtonPressed = useRef(false);
 
   // 앱 첫 시작시 히스토리 하나 추가(뒤로가기 종료 막기용)
   useEffect(() => {
     if (!sessionStorage.getItem('firstLoad')) {
       sessionStorage.setItem('firstLoad', 'true');
-      window.history.pushState(null, 'current', window.location.href);
+      window.history.pushState(null, 'current', '/');
     }
   }, []);
 
@@ -20,22 +22,17 @@ export const useCloseAppHandler = (delay = 1500) => {
     let timeout: any;
 
     const handleBackButtonEvent = () => {
-      if (!backButtonPressed.current) {
-        backButtonPressed.current = true;
+      if (!isBackButtonPressed.current) {
+        isBackButtonPressed.current = true;
         toast('한번 더 누르면 앱이 종료됩니다.', { duration: delay });
 
         timeout = setTimeout(() => {
-          window.history.pushState(null, 'current', window.location.href); // 히스토리 복구
-          backButtonPressed.current = false;
+          window.history.pushState(null, 'current', '/'); // 히스토리 복구
+          isBackButtonPressed.current = false;
         }, delay);
-      } else {
-        // 사용자가 지정된 시간 내에 다시 뒤로 가기를 누르면 앱 종료
-        window.history.go(-(window.history.length + 2));
       }
     };
-
     window.addEventListener('popstate', handleBackButtonEvent);
-
     return () => {
       window.removeEventListener('popstate', handleBackButtonEvent);
       if (timeout) {
