0d85d6b08aff397823797c7cd22808c0cb4b345e
woo3145 <lcwoo3145@gmail.com>
2024-03-22 10:36:54 +0900

    Fix pinch zoom for samsung browser

--- src/hooks/use-initialize-app.ts
+++ src/hooks/use-initialize-app.ts
@@ -41,7 +41,7 @@ export const useInitializeApp = () => {
   }, [timerMode, toggleTimerMode]);
 
   React.useEffect(() => {
-    const preventZoom = () => (event: TouchEvent) => {
+    const preventZoom = (event: TouchEvent) => {
       // 핀치 줌의 경우 두 개 이상의 이벤트가 발생
       if (event.touches.length > 1) {
         event.preventDefault();
